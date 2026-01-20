3a3197fa3c4e950e4d0e31f7c3b033dd5bcf20b6
Bisheka Weerathunga <weerathungabisheka@gmail.com>
2026-01-20 06:53:52 +0530

    Add files via upload

---
+++ Lab 2/IT24102555Lab2Q3.java
@@ -0,0 +1,12 @@
+public class IT24102555Lab2Q3{
+             public static void main (String[] args){
+ 
+             //Given sides of the triangle
+             double sideA=3.0;
+             double sideB=4.0;
+            //Calculate the hypotenuse
+            double hypotenuse=Math.hypot(sideA, sideB);
+
+        System.out.println("Length of the hypotenuse is::"+hypotenuse);
+   }
+}
